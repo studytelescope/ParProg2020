9fd02a5f85661f1c02aaf91915b691b54856e474
Sergey Lisitsyn <fox9508@ya.ru>
2020-10-07 21:33:14 +0300

    Prepare OpenMP4 main.cpp and tests

--- OpenMP4/src/main.cpp
+++ OpenMP4/src/main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <omp.h>
 
-double calc()
+double calc(uint32_t x_last, uint32_t num_threads)
 {
   return 0;
 }
@@ -34,10 +34,12 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  // Read arguments from input
+// Read arguments from input
+  uint32_t x_last = 0, num_threads = 0;
+  input >> x_last >> num_threads;
 
   // Calculation
-  double res = calc();
+  double res = calc(x_last, num_threads);
 
   // Write result
   output << std::setprecision(15) << res << std::endl;
