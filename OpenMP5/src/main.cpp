3eb4dac4c0101b7860025fd0c18d4047a1c6d630
Sergey Lisitsyn <fox9508@ya.ru>
2020-10-11 01:06:29 +0300

    Update tests and reading/writeng frames for OpenMP5

--- OpenMP5/src/main.cpp
+++ OpenMP5/src/main.cpp
@@ -44,6 +44,7 @@ int main(int argc, char** argv)
     for (uint32_t x = 0; x < xSize; x++)
     {
       input >> inputFrame[y*xSize + x];
+      inputFrame[y*xSize + x] -= '0';
     }
   }
 
@@ -55,7 +56,8 @@ int main(int argc, char** argv)
   {
     for (uint32_t x = 0; x < xSize; x++)
     {
-    output << " " << outputFrame[y*xSize + x];
+      outputFrame[y*xSize + x] += '0';
+      output << " " << outputFrame[y*xSize + x];
     }
     output << "\n";
   }
